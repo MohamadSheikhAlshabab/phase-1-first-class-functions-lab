43faa74d5e285bfbaaac15d79b0bed914be21ebc
MohamadSheikhAlshabab <alshabab.moh@gmail.com>
2023-10-01 19:34:25 +0300

    Compeleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(){
+    const newDrivers = drivers;
+    return newDrivers.slice(0,2)
+}
+
+function returnLastTwoDrivers(){
+    const newDrivers = drivers;
+    return newDrivers.slice(2)
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(fare){
+    return function fareMultiplier(){  
+        let multiplier = 0; 
+        return multiplier = fare * fare;
+    }
+}
+
+const fareDoubler = (fare) => {
+    function multiplierFareDoubler(){  
+        let multiplier = 0; 
+        multiplier = fare * 2;
+        return multiplier
+    }
+    return multiplierFareDoubler()
+}
+
+const fareTripler =  (fare) => {
+    // solution #1
+
+    // function multiplierFareTripler(){  
+    //     let multiplier = 0; 
+    //     multiplier = fare * 3;
+    //     return multiplier
+    // }
+    // return multiplierFareTripler()
+
+    // solution #2
+    
+    return fareDoubler(fare) + fare;
+}
+
+function selectDifferentDrivers(drivers,selectingDrivers){
+    return selectingDrivers()
+}
